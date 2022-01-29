d616edd29582da5d9ba1e07150e388449d27032b
Amman Bhat <amli1996@hotmail.com>
2022-01-29 18:19:07 -0500

    restructured image component for generated images

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,10 +1,33 @@
-import { ConstructionOutlined } from "@mui/icons-material";
-import { TextField, Grid, Container, Button, Box } from "@mui/material";
+
+import {
+  TextField,
+  Grid,
+  Container,
+  Button,
+  Typography,
+  Card,
+  CardMedia,
+  Box,
+} from "@mui/material";
 import Image from "material-ui-image";
 import memesData from "../memesData";
 import { useState } from "react";
 
 const styles = {
+  topText: {
+    position: "absolute",
+    top: "31%",
+    width: "100%",
+    textAlign: "center",
+    color: "black",
+    backgroundColor: "none",
+    marginLeft: 70
+  },
+
+  bottomText: {
+    textAlign: "center",
+  },
+
   formButton: {
     height: 50,
     background: "linear-gradient(to top, #6a3093, #a044ff)",
@@ -13,48 +36,62 @@ const styles = {
     color: "white",
     marginBottom: 50,
   },
+
+  imageContainer: {
+    position: "relative",
+    marginLeft: "80px",
+    width: "100%",
+  },
+
   generatedImages: {
-    objectFit: 'contain'
-  }
+    backgroundSize: "cover",
+    border: "3px solid red",
+  },
 };
 
-
-
-
 const Meme = () => {
-  
-
-  // const [memeImage, setMemeImage] = useState("");
-
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
-  const [allMemeImage, setAllMemeImage] = useState(memesData)
+  const [allMemeImage, setAllMemeImage] = useState(memesData);
 
   const getMemeImage = () => {
     const memesArray = allMemeImage.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
     const url = memesArray[randomNumber].url;
-    setMeme(prevMeme => ({
-      ...prevMeme, randomImage: url
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
     }));
   };
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  };
 
   return (
     <>
-      {/*  No need to use form since we are not submitting a form using a button */}
+      {/*  No need to use form element */}
       <Container>
-        <Grid container rowSpacing={3} columnSpacing={10} mt={3}>
+        <Grid container rowSpacing={3} columnSpacing={10} mt={3} justifyContent={'center'}>
           <Grid item xs={12} lg={6}>
             <TextField
               fullWidth
               id="filled-basic"
               label="Insert top text here"
               variant="filled"
+              name="topText"
+              value={meme.topText}
+              onChange={handleChange}
             />
           </Grid>
 
@@ -64,6 +101,9 @@ const Meme = () => {
               id="filled-basic"
               label="Insert bottom text here"
               variant="filled"
+              name="bottomText"
+              value={meme.bottomText}
+              onChange={handleChange}
             />
           </Grid>
 
@@ -77,13 +117,12 @@ const Meme = () => {
               Get a new meme image
             </Button>
           </Grid>
-        </Grid>
 
-        <Image
-          src={meme.randomImage}
-          alt="random-images"
-          style={styles.generatedImages}
-        />
+          <Card style={styles.imageContainer} >
+            <CardMedia style={styles.generatedImages} component='img' alt="generated-images" image={ meme.randomImage}/>
+        </Card>
+        <Typography style={styles.topText}>This is the top text</Typography>
+        </Grid>
       </Container>
     </>
   );
