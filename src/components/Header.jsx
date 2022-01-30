2efaa9d30a70f7cf426acc48305abb113ae04ee7
Amman Bhat <amli1996@hotmail.com>
2022-01-30 16:30:38 -0500

    removed material ui styling. added plain css styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,18 @@
-import { AppBar, Box, Toolbar, Typography, Container } from "@mui/material";
-import trollFace from "../images/Troll-Face.svg";
-
-const styles = {
-  appBar: {
-    position: "static",
-    background: "linear-gradient(to top, #6a3093, #a044ff)",
-  },
-  title: {
-    marginLeft: 8,
-    flexGrow: 1,
-    fontSize: 21,
-    fontWeight: 700,
-  },
-  toolBar: {
-    minHeight: 80,
-  },
-};
+import "../style.css";
+import trollFace from "../images/troll-face.png";
 
 const Header = () => {
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar style={styles.appBar}>
-          <Container maxWidth='xl'>
-            <Toolbar style={styles.toolBar}>
-              <img src={trollFace} alt="troll-svg-icon" />
-              <Typography variant="h6" style={styles.title}>
-                Meme Generator
-              </Typography>
-              <Typography variant="body1">React Course - Project 3</Typography>
-            </Toolbar>
-          </Container>
-        </AppBar>
-      </Box>
+      <header className="header">
+        <img
+          src={trollFace}
+          className="header--image"
+          alt="logo"
+        />
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">React Course - Project 3</h4>
+      </header>
     </>
   );
 };
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,50 +1,7 @@
 
-import {
-  TextField,
-  Grid,
-  Container,
-  Button,
-  Typography,
-  Card,
-  CardMedia,
-  Box,
-} from "@mui/material";
-import Image from "material-ui-image";
 import memesData from "../memesData";
 import { useState } from "react";
-
-const styles = {
-  topText: {
-    position: "absolute",
-    top: "31%",
-    width: "100%",
-    textAlign: "center",
-    color: "black",
-    backgroundColor: "none",
-    marginLeft: 70,
-  },
-
-  bottomText: {
-    textAlign: "center",
-  },
-
-  formButton: {
-    height: 50,
-    background: "linear-gradient(to top, #6a3093, #a044ff)",
-    fontSize: 16,
-    fontWeight: 420,
-    color: "white",
-    marginBottom: 50,
-  },
-  imageContainer: {
-    justifyContent: 'center',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain',
-    maxWidth: '100%',
-    height: 600,
-    marginBottom: 30
-  },
-};
+import '../style.css';
 
 const Meme = () => {
   const [meme, setMeme] = useState({
@@ -77,65 +34,36 @@ const Meme = () => {
 
   return (
     <>
-      {/*  No need to use form element */}
-      <Container>
-        <Grid
-          container
-          rowSpacing={3}
-          columnSpacing={10}
-          mt={3}
-          justifyContent={"center"}
-        >
-          <Grid item xs={12} lg={6}>
-            <TextField
-              fullWidth
-              id="filled-basic"
-              label="Insert top text here"
-              variant="filled"
-              name="topText"
-              value={meme.topText}
-              onChange={handleChange}
-            />
-          </Grid>
+      <main>
+        <div className="form">
+          <input
+            className="form--input"
+            placeholder="Top text"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
+          />
 
-          <Grid item xs={12} lg={6}>
-            <TextField
-              fullWidth
-              id="filled-basic"
-              label="Insert bottom text here"
-              variant="filled"
-              name="bottomText"
-              value={meme.bottomText}
-              onChange={handleChange}
-            />
-          </Grid>
+          <input
+            type="text"
+            placeholder="Bottom text"
+            className="form--input"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
 
-          <Grid item xs={12} lg={12}>
-            <Button
-              onClick={getMemeImage}
-              fullWidth
-              variant="dashed"
-              style={styles.formButton}
-            >
-              Get a new meme image
-            </Button>
-          </Grid>
-        </Grid>
-        <Container maxWidth={'md'}>
-          
-        <Grid
-          container
-          style={styles.imageContainer}
-          sx={{ backgroundImage: `url(${meme.randomImage})`}}
-          >
-          <Grid item>
-            <Typography>Hello</Typography> 
-            </Grid>
-            {/* Add another trypography, use grid container direction vertical to set
-            the two texts apart from each other. top and bottom. */}
-        </Grid>
-          </Container>
-      </Container>
+          <button className="form--button" onClick={getMemeImage}>
+            Get a new meme image
+          </button>
+        </div>
+
+        <div className="meme">
+          <img src={meme.randomImage} className="meme--image" />
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
+      </main>
     </>
   );
 };
